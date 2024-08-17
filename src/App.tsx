bfea81993eee40e83e4ec40164813ee73601c8c5
Leonardo Cunha <lleon.sc27@gmail.com>
2024-08-17 18:03:07 -0300

    NavBar with responsive design and toggle menu

--- src/App.tsx
+++ src/App.tsx
@@ -1,26 +1,42 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+// import React from 'react';
+// import logo from './logo.svg';
+// import './App.css';
 
-function App() {
+import { appendFile } from "fs";
+
+// function App() {
+//   return (
+//     <div className="App">
+//       <header className="App-header">
+//         <img src={logo} className="App-logo" alt="logo" />
+//         <p>
+//           Edit <code>src/App.tsx</code> and save to reload.
+//         </p>
+//         <a
+//           className="App-link"
+//           href="https://reactjs.org"
+//           target="_blank"
+//           rel="noopener noreferrer"
+//         >
+//           Learn React
+//         </a>
+//       </header>
+//     </div>
+//   );
+// }
+
+// export default App;
+
+import './App.css'
+import NavBar from "./components/NavBar";
+
+const ArtWall = () => {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+
+    <div className="components">
+      <NavBar/>
     </div>
   );
-}
+};
 
-export default App;
+export default ArtWall;
---
+++ src/components/NavBar/index.tsx
@@ -0,0 +1,37 @@
+import React, { useState } from 'react';
+import './styles.css';
+import ArtWallLogo from './ArtWallLogo.svg';
+
+const NavBar: React.FC = () => {
+    const [isOpen, setIsOpen] = useState(false);
+
+    const toggleMenu = () => {
+        setIsOpen(!isOpen);
+    };
+
+    return (
+        <header className="header">
+            <div className="logo">
+                <img className="logo-img" src={ArtWallLogo} alt="ArtWall Logo" />
+            </div>
+
+            <div className={`toggle-menu ${isOpen ? 'active' : ''}`} onClick={toggleMenu}>
+                <div className="bar"></div>
+                <div className="bar"></div>
+                <div className="bar"></div>
+            </div>
+
+            <nav className={`menu-nav ${isOpen ? 'show' : ''}`}>
+                <ul className="menu">
+                    <li><a className='item-menu' href="#home">Home</a></li>
+                    <li><a className='item-menu' href="#produtos">Produtos</a></li>
+                    <li><a className='item-menu' href="#sobre-nos">Sobre Nós</a></li>
+                    <li><a className='item-menu' href="#contato">Contato</a></li>
+                </ul>
+            </nav>
+        </header>
+    )
+
+}
+
+export default NavBar
