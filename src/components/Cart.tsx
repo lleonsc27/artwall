fdde8e39c1b7111dd23570cfcdddf4ae6141b045
Leonardo Cunha <lleon.sc27@gmail.com>
2024-10-23 19:47:55 -0300

    empty cart css

--- src/components/Cart.tsx
+++ src/components/Cart.tsx
@@ -2,6 +2,7 @@ import React, { useContext, useState } from "react";
 import { CartContext } from "../context/CartContext";
 import CheckoutForm from "./CheckoutForm";
 import "../assets/styles/cart.css";
+import { Link } from "react-router-dom";
 
 const Cart: React.FC = () => {
   const cartContext = useContext(CartContext);
@@ -25,32 +26,38 @@ const Cart: React.FC = () => {
     <div className="cart">
       <h1>Seu Carrinho</h1>
       {cart.length === 0 ? (
-        <p>O carrinho está vazio</p>
+        <div className="cart-empty">
+          <p>O carrinho está vazio</p>
+          <Link className="products-redirect" to={`/products`}>ver produtos</Link>
+        </div>
       ) : (
         <div className="cart-summary">
           <div className="cart-items-list">
+            {cart.map((item) => (
+              <div key={item.id} className="cart-items">
+                <img
+                  className="cart-item-img"
+                  src={item.imageUrl}
+                  alt={item.name}
+                />
+                <div className="cart-item">
+                  <div className="cart-item-info">
+                    <p className="cart-item-name">{item.name} </p>
+                    <p className="cart-item-price">
+                      {item.quantity} x R${item.price.toFixed(2)}
+                    </p>
+                  </div>
 
-          {cart.map((item) => (
-            <div key={item.id} className="cart-items">
-              <img
-                className="cart-item-img"
-                src={item.imageUrl}
-                alt={item.name}
-              />
-              <div className="cart-item">
-
-
-              <div className="cart-item-info">
-                <p className="cart-item-name">{item.name} </p>
-                <p className="cart-item-price">
-                  {item.quantity} x R${item.price.toFixed(2)}
-                </p>
+                  <button
+                    type="button"
+                    className="cart-item-remove-button"
+                    onClick={() => removeItem(item.id)}
+                  >
+                    Remover
+                  </button>
+                </div>
               </div>
-
-              <button type="button" className="cart-item-remove-button" onClick={() => removeItem(item.id)}>Remover</button>
-              </div>
-            </div>
-          ))}
+            ))}
           </div>
           <div className="cart-checkout">
             <div className="cart-checkout-info">
